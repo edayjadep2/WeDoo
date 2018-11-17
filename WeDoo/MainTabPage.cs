671192b5117751c627d990e0d910f400be15306f
eddy wijaya <eddy.wijaya@aia.com>
2018-11-17 10:44:54 +0800

    first

---
+++ WeDoo/MainTabPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace WeDoo
+{
+    public class MainTabPage : TabbedPage
+    {
+        public MainTabPage()
+        {
+
+          
+        }
+    }
+}
+
---
+++ WeDoo/TabbedPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WeDoo
+{
+    public partial class TabbedPage : TabbedPage
+    {
+        public TabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
